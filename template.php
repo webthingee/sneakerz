52f42afa6d1feee8436d339bbe621ba496252eeb
Sean.Lange <sean@webthingee.com>
2012-10-21 15:09:28 -0700

    initial import

---
+++ template.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Include Once for initialization of elements for twitter bootstrap markup,
+ * look and feel.
+ */
+
+include_once(drupal_get_path('theme', 'sneakerz') . '/includes/breadcrumb.inc');
+include_once(drupal_get_path('theme', 'sneakerz') . '/includes/error.inc');
+include_once(drupal_get_path('theme', 'sneakerz') . '/includes/menu.inc');
---
+++ templates/page.tpl.php
@@ -0,0 +1,100 @@
+<div id="page">
+  <header id="header" role="banner">
+    <?php if ($secondary_menu): ?>
+      <div class="navbar navbar-inverse">
+        <div class="navbar-inner">
+          <div class="container">
+            <a class="btn btn-navbar" data-toggle="collapse" data-target=".nav-collapse">
+              <span class="icon-bar"></span>
+              <span class="icon-bar"></span>
+              <span class="icon-bar"></span>
+            </a>
+            <div class="nav-collapse">
+              <?php print theme('links__system_main_menu', array('links' => $secondary_menu, 'attributes' => array('id' => 'main-menu', 'class' => array('links', 'inline', 'clearfix')), 'heading' => t('Main menu'))); ?>
+            </div> <!-- /.nav-collapse -->
+          </div> <!-- /.container -->
+        </div> <!-- /.navbar-inner -->
+      </div> <!-- /.navbar -->
+    <?php endif; ?>
+
+    <?php if ($logo || $site_slogan): ?>
+      <hgroup id="name-and-slogan">
+        <?php if ($logo): ?>
+          <a href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>" rel="home" id="logo"><img src="<?php print $logo; ?>" alt="<?php print t('Home'); ?>" /></a>
+        <?php endif; ?>
+
+        <?php if ($site_slogan): ?>
+          <h2 id="site-slogan"><?php print $site_slogan; ?></h2>
+        <?php endif; ?>
+      </hgroup><!-- /#name-and-slogan -->
+    <?php endif; ?>
+
+    <?php print render($page['header']); ?>
+
+    <div id="navigation">
+
+      <?php if ($main_menu): ?>
+        <div class="navbar navbar-inverse">
+          <div class="navbar-inner">
+            <div class="container">
+              <a class="btn btn-navbar" data-toggle="collapse" data-target=".nav-collapse">
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+              </a>
+              <?php if ($title): ?><a class="brand" href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>" rel="home"><?php print $site_name; ?></a><?php endif; ?>
+              <div class="nav-collapse">
+                <?php print theme('links__system_main_menu', array('links' => $main_menu, 'attributes' => array('id' => 'main-menu', 'class' => array('links', 'inline', 'clearfix')), 'heading' => t('Main menu'))); ?>
+              </div> <!-- /.nav-collapse -->
+            </div> <!-- /.container -->
+          </div> <!-- /.navbar-inner -->
+        </div> <!-- /.navbar -->
+      <?php endif; ?>
+
+      <?php print render($page['navigation']); ?>
+
+    </div><!-- /#navigation -->
+
+  </header>
+
+  <div id="main">
+
+    <div id="content" class="column" role="main">
+      <?php print render($page['highlighted']); ?>
+      <?php print $breadcrumb; ?>
+      <a id="main-content"></a>
+      <?php print render($title_prefix); ?>
+      <?php if ($title): ?>
+        <h1 class="title" id="page-title"><?php print $title; ?></h1>
+      <?php endif; ?>
+      <?php print render($title_suffix); ?>
+      <?php print $messages; ?>
+      <?php print render($tabs); ?>
+      <?php print render($page['help']); ?>
+      <?php if ($action_links): ?>
+        <ul class="action-links"><?php print render($action_links); ?></ul>
+      <?php endif; ?>
+      <?php print render($page['content']); ?>
+      <?php print $feed_icons; ?>
+    </div><!-- /#content -->
+
+    <?php
+      // Render the sidebars to see if there's anything in them.
+      $sidebar_first  = render($page['sidebar_first']);
+      $sidebar_second = render($page['sidebar_second']);
+    ?>
+
+    <?php if ($sidebar_first || $sidebar_second): ?>
+      <aside class="sidebars">
+        <?php print $sidebar_first; ?>
+        <?php print $sidebar_second; ?>
+      </aside><!-- /.sidebars -->
+    <?php endif; ?>
+
+  </div><!-- /#main -->
+
+  <?php print render($page['footer']); ?>
+
+</div><!-- /#page -->
+
+<?php print render($page['bottom']); ?>
